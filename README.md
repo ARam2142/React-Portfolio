2180a07544f8b562b0823f297dd8f9f8a23115ed
Aram2142 <andresaaramirez@aol.com>
2020-11-03 20:49:39 -0500

    starting with header on index page

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # React-Portfolio
+
+Updated portfolio featuring 6 total projects
+
+Use React
+
+A Header component that appears on multiple pages
+
+A single Project component that will be used multiple times on a single page
+
+Navigation with React Router, dynamic rendering, or another third part router
+
+A Footer component that appears on multiple pages
+
+Update GitHub profile with pinned repositories featuring those same projects
+
+Deploy this site to GitHub Pages using the Create React App docs for deployment.
+
+
+
---
+++ portfolio/src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-light" style={{'background-color': '#e3f2fd'}}>
+            <span class="nav justify-content-start"><p class="h1 text-secondary"> Andrés Ramírez</p></span>
+            <ul class="d-flex nav justify-content-end h4">
+                <li class="nav-item">
+                    <Link to="/home" className={window.location.pathname === "/home" ? "nav-link active" : "nav-link"}>
+                        Home
+                    </Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                        Portfolio
+                    </Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/contact" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
