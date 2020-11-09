51ac375d9e13856b9b165a08896a3c52d42ecf07
Aram2142 <andresaaramirez@aol.com>
2020-11-09 00:26:33 -0500

    got files back

---
+++ portfolio/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer/Footer.js";
+
+function App() {
+  return (
+    <Router>
+      <div>        
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/portfolio" component={Portfolio}/>
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+        <Footer/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+
+function Footer() {
+    return (
+        <nav className="navbar fixed-bottom navbar-dark bg-dark justify-content-center">
+            <span className="navbar-brand footer-font blue align-middle">
+            Copyright &copy;2020 | Andrés Ramírez</span>
+        </nav>
+    )
+}
+
+export default Footer;
