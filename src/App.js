a627bda7fcf08880cb349e5c5100217e76d49670
Aram2142 <andresaaramirez@aol.com>
2020-11-11 21:52:00 -0500

    added container

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-//import Nav from "./components/Nav/Nav.js";
 import Footer from "./components/Footer/Footer.js";
+import Container from "./components/Container/Container.js";
+import Nav from "./components/Nav/Nav.js";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Nav/>  */}
+        <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/portfolio" component={Portfolio}/>
           <Route exact path="/contact" component={Contact} />
         </Switch>
+        <Container/>
         <Footer/>
       </div>
     </Router>
