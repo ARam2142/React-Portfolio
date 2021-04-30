925e3a1ae4f576e90ee273e9e9c643185b0693d7
Aram2142 <andresaaramirez@aol.com>
2021-04-29 20:53:18 -0400

    small changes to home and contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 import Nav from "react-bootstrap/Nav";
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
                                 <div className="card-body">
                                     <h5 className="card-title text-center">Web Developer</h5>
                                     <p className="card-text">Hola, My name is Andrés, I am graduate from the University of New Hampshire Full Stack Coding Boot Camp.
-                                    I have acquired skills in HTML, CSS, Javascript, JQuery, Bootstrap, Node.js, Express, Express Handlebars, MySQL, sequelize, MongoDB, Mongoose, and React.
+                                    I have acquired skills in HTML, CSS, Javascript, JQuery, Bootstrap, Node.js, Express, Express Handlebars, MySQL, sequelize, MongoDB, Mongoose, Typescript and React.
                                     When I am not coding, I like to travel, play video games, and watch movies. While you are here, take a look at my awesome applications that I have created.
                                     If you would like to get to know me more feel free to reach out to me.
                                     
