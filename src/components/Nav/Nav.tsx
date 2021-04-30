9b41d060699b7fc99431d8c9865f1e283e19322e
Aram2142 <andresaaramirez@aol.com>
2021-04-29 20:59:03 -0400

    successfully made a responsive navbar with burger menu

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,27 +1,35 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import './style.css'
 
 function Nav() {
+    const [isMobile, setMobile] = useState(false)
     return (
-        <nav className="navbar navbar-light" style={{ backgroundColor: '#e3f2fd' }}>
-            <span className="nav justify-content-start"><p className="h1 text-secondary"> Andrés Ramírez</p></span>
-            <ul className="d-flex nav justify-content-end h4">
-                <li className="nav-item">
-                    <Link to="/home" className={window.location.pathname === "/" || window.location.pathname === "/home" ? "nav-link active" : "nav-link"}>
-                        Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/projects" className={window.location.pathname === "/projects" ? "nav-link active" : "nav-link"}>
-                        Projects
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/contact" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
-                        Contact
-                    </Link>
-                </li>
+        <nav className="navbar">
+            <h1 className="text-light"> Andrés Ramírez</h1>
+            <ul className={isMobile ? "nav-links-mobile" : "nav-links"}
+                onClick={() => setMobile(false)}
+            >
+                <Link to="/home" className={"/home" ? "nav-link active" : "nav-link"}>
+                    <li className="">Home</li>
+                </Link>
+                <Link to="/projects" className={"/projects" ? "nav-link active" : "nav-link"}>
+                    <li className="">Projects</li>
+                </Link>
+                <Link to="/contact" className={"/contact" ? "nav-link active" : "nav-link"}>
+                    <li className="">Contact</li>
+                </Link>
             </ul>
+            <button className="mobile-menu-icon"
+            onClick={()=> {setMobile(!isMobile)}}
+            >
+
+                {isMobile ? (
+                    <i className="fas fa-times"></i>
+                ) : (
+                    <i className="fas fa-bars"></i>
+                )}
+            </button>
         </nav>
     )
 }
