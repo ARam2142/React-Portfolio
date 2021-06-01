40fed1993bbf8dab863035a18fc1bee70a5bb4ea
Aram2142 <andresaaramirez@aol.com>
2021-05-31 23:37:44 -0400

    added dashes between phonenumber

--- src/utils/ContactData.tsx
+++ src/utils/ContactData.tsx
@@ -1,12 +1,12 @@
 const ContactData = [
     {
-        phone: 13514442871,
+        phone: '1-351-444-2871',
         email: 'andresaaramirez730@gmail.com',
         website: 'https://github.com/ARam2142',
         linkedin: 'https://www.linkedin.com/in/andres-ramirez-8b07487b/',
         resume: 'https://drive.google.com/file/d/1VBcI0io6YjjRP_pwa7Z66E2FSxpM9wfi/view?usp=sharing'
     }
 ];
-console.log(ContactData)
+
 
 export default ContactData;
